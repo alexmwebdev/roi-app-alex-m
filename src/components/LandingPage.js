8c079c3d46c305a2f4be20b89d18c13e84d7d029
Alex Maniago <alexandermaniago@gmail.com>
2024-05-23 09:07:25 +1000

    Fixed the wording of the home page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { ImageBackground, StyleSheet, Text, Image, View } from "react-native";
-import { Link } from 'react-router-native';
-import { Headline } from 'react-native-paper';
+import { Headline, Button } from 'react-native-paper';
+import { Link, BrowserRouter, Route, Router, Routes, Switch } from "react-router-dom";
+import AddPerson from './AddPerson';
 
 const image = { uri: "../images/roi-background.png" };
 const logo = { url: "./Roilogo.png"}
+
+
 // missing image <Image source={require('./images/logo_example01.png')} />
 const App = () => (
   <View style={styles.container}>
     <ImageBackground source={require('../images/roi-background.png')} resizeMode="cover" style={styles.image}>
-    <Headline style={styles.headline}>Welcome to the ROI App!</Headline>
+    <Headline style={styles.headline}>Welcome to the ROI Staff Contact App!</Headline>
       <Text style={styles.text}>
-          You can add a new staff contact, view or edit an existing staff contact and view the departments that each staff contact is in currently.
+          Select from the menu options below to add a new staff contact, view or edit an existing staff contact and view the departments that each staff contact is in currently.
       </Text>
+
       
     </ImageBackground>
   </View>
